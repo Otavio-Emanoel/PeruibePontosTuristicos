4a5d1e748ecb0cab03a52c8390ae7ec9ccfae075
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-03-28 20:33:00 -0300

    Criação do modal dos dev

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,3 +26,33 @@ document.addEventListener("DOMContentLoaded", function () {
         updateCarousel();
     }, 5000);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modalDevsOpen = document.querySelector("#abrir-modal-devs");
+    const modalDevs = document.querySelector(".modalbg");
+    const modalDevsClose = document.querySelector(".close_modal");
+
+    if (!modalDevsOpen) {
+        console.error("Erro: Botão #abrir-modal-devs não encontrado no DOM!");
+        return;
+    }
+
+    // Abrir o modal
+    modalDevsOpen.addEventListener("click", function () {
+        modalDevs.style.display = "flex";
+    });
+
+    // Fechar o modal
+    modalDevsClose.addEventListener("click", function () {
+        modalDevs.style.display = "none";
+        
+    });
+
+    // Fechar o modal ao clicar fora dele
+    modalDevs.addEventListener("click", function (e) {
+        if (e.target === modalDevs) {
+            modalDevs.style.display = "none";
+            
+        }
+    });
+});
